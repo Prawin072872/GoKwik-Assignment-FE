4adb89f12fe764e4ae2e3afa9958933609c4aa36
Prawin072872 <152619511+Prawin072872@users.noreply.github.com>
2024-05-30 18:38:21 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div>Protected by reCaptcha. Google</div>
-            <div><a href='#' className="blue-link">Privacy Policy</a> & <a href='#' className="blue-link">Terms of Service</a> apply.</div>
+            <div><a href='https://static.googleusercontent.com/media/www.google.com/en//intl/en/policies/privacy/google_privacy_policy_en.pdf' className="blue-link">Privacy Policy</a> & <a href='https://policies.google.com/terms?hl=en-US' className="blue-link">Terms of Service</a> apply.</div>
             
         </footer>
     );
