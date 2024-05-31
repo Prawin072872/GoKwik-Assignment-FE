985342b72821fef0fb65c2346834e8f087320944
prawin <prawin.j07@gmail.com>
2024-05-31 11:44:25 +0530

    Layout fixes final

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ function App() {
         <footer>
           <p>Protected by reCaptcha. Google</p>
           <p>
-            <a href="#" className="blue-link">Privacy Policy</a> & <a href="#" className="blue-link">Terms of Service</a> apply.
+            <a href="https://static.googleusercontent.com/media/www.google.com/en//intl/en/policies/privacy/google_privacy_policy_en.pdf" className="blue-link">Privacy Policy</a> & <a href="https://policies.google.com/terms?hl=en-US" className="blue-link">Terms of Service</a> apply.
           </p>
         </footer>
       </div>
